8a098db8fce698d4127776619432cbb4488e6bdd
roddev-v <iovescu.dani@gmail.com>
2020-04-14 03:09:27 +0300

    crude representation of the concept

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-let test = [10,2,4,6,9,12,0];
+let test = [10, 2, 4, 6, 9, 12, 0, 22, 100, 21, 32];
+// initial array
+printArray(test, 'Initial array');
+
+//Sorting 
+bubbleSort(test);
+
+//Sorting process 
+printArray(test, 'Final array');
 
 function bubbleSort(input) {
     let n = input.length;
-    for(let i = 0 ; i < n - 1; i++) {
-        for(let j = 0; j < n - i - 1; j++) {
-            if(input[j] > input[j+1]) {
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            printArray(input, `Step i=${i} j=${j}`, j);
+            if (input[j] > input[j + 1]) {
                 let aux = input[j];
-                input[j] = input[j+1];
-                input[j+1] = aux;
+                input[j] = input[j + 1];
+                input[j + 1] = aux;
             }
         }
     }
 }
 
-function printArray(input){
-    input.forEach(element => {
-        console.log(element);
+function printArray(input, label, highlight) {
+    const resultArea = document.getElementById("resultArea");
+    const container = document.createElement('div');
+    const info = document.createElement('div');
+
+    info.innerHTML = label;
+    info.className = 'label';
+    container.className = 'container';
+    container.appendChild(info);
+    input.forEach((element, index) => {
+        const div = document.createElement('div');
+        div.innerHTML = element;
+        div.className = 'item';
+        if (index == highlight || index == highlight + 1) {
+            div.classList.add('highlight');
+        }
+        container.appendChild(div);
     });
+    resultArea.appendChild(container);
 }
